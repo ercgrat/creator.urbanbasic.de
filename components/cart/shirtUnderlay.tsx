0de452fac2e50d3d977a502e86ea524a41f8332d
Eric Gratta <eric.gratta@gmail.com>
2020-11-26 15:55:05 -0500

    Fix color flash.

--- components/cart/shirtUnderlay.tsx
+++ components/cart/shirtUnderlay.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ColorMap, Design, DesignColor } from "../../model/Cart";
 
 export default React.memo(function ShirtUnderlay(props: {
@@ -6,10 +6,33 @@ export default React.memo(function ShirtUnderlay(props: {
   color: DesignColor;
   className?: string;
 }) {
-  let heatheredImage = "url(/images/heather.png) ";
-  if (props.color !== DesignColor.oxfordGrey) {
-    heatheredImage = "";
-  }
+  const [heatheredImage, setHeatheredImage] = useState("");
+
+  const [color, setColor] = useState("");
+  const shirtUrl = `/images/tshirt-${props.shirtPosition}.png`;
+  useEffect(() => {
+    // Wait until the shirt image loads before applying color or heathering
+    // This prevents the big square flash of color when changing sides and shirts
+
+    // If switching shirt sides, hide the color background
+    if (ColorMap[props.color].color === color) {
+      setColor("");
+    }
+    setHeatheredImage("");
+    const img = new Image();
+    const listener = () => {
+      setColor(ColorMap[props.color].color);
+      if (props.color === DesignColor.oxfordGrey) {
+        setHeatheredImage("url(/images/heather.png) ");
+      }
+    };
+    img.addEventListener("load", listener);
+    img.src = shirtUrl;
+
+    return () => {
+      img.removeEventListener("load", listener);
+    };
+  }, [shirtUrl, props.color]);
 
   return (
     <>
@@ -17,12 +40,10 @@ export default React.memo(function ShirtUnderlay(props: {
         className={props.className}
         style={{
           position: "relative",
-          background: `${heatheredImage} center center / 100% 100% no-repeat ${
-            ColorMap[props.color].color
-          }`,
+          background: `${heatheredImage} center center / 100% 100% no-repeat ${color}`,
           zIndex: 0,
-          top: '1px',
-          height: 'calc(100% - 1px)'
+          top: "1px",
+          height: "calc(100% - 1px)",
         }}
       ></div>
       <div
@@ -30,7 +51,7 @@ export default React.memo(function ShirtUnderlay(props: {
         style={{
           position: "absolute",
           top: 0,
-          background: `url(/images/tshirt-${props.shirtPosition}.png) center center / 100% 100% no-repeat`,
+          background: `url(${shirtUrl}) center center / 100% 100% no-repeat`,
           zIndex: 1,
         }}
       />
